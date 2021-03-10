03e2bfc89fd73fb2276639dca916d6054a5017cb
Roger Qiu <roger.qiu@polyhack.io>
2021-03-11 02:08:35 +1100

    Checking for whether production dependencies exist before trying to copy over

--- default.nix
+++ default.nix
@@ -17,7 +17,9 @@ let
     # copy only the dist
     cp -r ${utils.node2nixDev}/lib/node_modules/${utils.node2nixDev.packageName}/dist $out/lib/node_modules/${utils.node2nixDev.packageName}/
     # copy over the production dependencies
-    cp -r ${utils.node2nixProd}/lib/node_modules $out/lib/node_modules/${utils.node2nixDev.packageName}/
+    if [ -d "${utils.node2nixProd}/lib/node_modules" ]; then
+      cp -r ${utils.node2nixProd}/lib/node_modules $out/lib/node_modules/${utils.node2nixDev.packageName}/
+    fi
     # create symlink to the deployed executable folder, if applicable
     if [ -d "${utils.node2nixDev}/lib/node_modules/.bin" ]; then
       cp -r ${utils.node2nixDev}/lib/node_modules/.bin $out/lib/node_modules/
