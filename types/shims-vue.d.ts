0bbf6702d7c3a49c78b6d1797d04797b147f40ba
Roger Qiu <roger.qiu@polyhack.io>
2021-03-10 20:04:47 +1100

    Optimising the build

--- types/shims-vue.d.ts
+++ types/shims-vue.d.ts
@@ -1,6 +1,5 @@
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-
-  const component: DefineComponent
-  export default component
+  import { ComponentOptions } from 'vue';
+  const Component: ComponentOptions;
+  export default Component;
 }
